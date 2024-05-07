aa8d3804c23f062f81ea9ba2671c6f78d0dc7508
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-05-07 00:07:37 -0600

    add first commit #hello_swift_ui

---
+++ hello_swift_ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello_swift_ui
+//
+//  Created by Julio Ramirez Rodriguez on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello_swift_ui/hello_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swift_uiApp.swift
+//  hello_swift_ui
+//
+//  Created by Julio Ramirez Rodriguez on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
